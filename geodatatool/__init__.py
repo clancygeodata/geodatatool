5dec8298d61a1654f84e8399482986e42dbb2496
clancy <clancy.geodata@gmail.com>
2021-03-20 01:27:42 -0400

    Bump version: 0.0.3 → 0.0.4

--- geodatatool/__init__.py
+++ geodatatool/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Clancy Oliver"""
 __email__ = 'clancy.geodata@gmail.com'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from .geodatatool import *
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/clancygeodata/geodatatool',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
